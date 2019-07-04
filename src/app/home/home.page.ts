a0bb6b1109611a09618e3363781a9b4c1616d2ea
AizenSousuke <puteranikdimas@yahoo.com.sg>
2019-07-04 11:05:33 +0800

    Forms working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public totalExpenses : number = 0;
+  public expenses = [
+    {"title": "Food", "value": 200},
+    {"title": "Drinks", "value": 200},
+    {"title": "Parties", "value": 200},
+    {"title": "Transport", "value": 200},
+    {"title": "Entertainment", "value": 200},
+  ];
+  BudgetPlannerForm = new FormGroup(
+    {
+        title: new FormControl(''),
+        value: new FormControl(''),
+    }
+  );
 
+  constructor() {
+
+  }
+
+  onAdd() {
+
+    console.log("Submitted");
+  }
+
+  onDelete(item) {
+    this.expenses.splice(item, 1);
+    console.log("Deleted " + item);
+  }
+
+  onClear() {
+    
+    console.log("Cleared");
+  }
 }
