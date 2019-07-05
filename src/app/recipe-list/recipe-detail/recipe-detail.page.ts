fdae1d635dbc8c6a00f0c7f9f2076a789d3a29ac
AizenSousuke <puteranikdimas@yahoo.com.sg>
2019-07-05 19:35:26 +0800

    Back now works from detail page

--- src/app/recipe-list/recipe-detail/recipe-detail.page.ts
+++ src/app/recipe-list/recipe-detail/recipe-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeListPage } from '../recipe-list.page';
+import { Location } from '@angular/common';
 
 
 @Component({
@@ -12,8 +13,16 @@ export class RecipeDetailPage implements OnInit {
   recipeName : string = '';
 
   constructor(private _route : ActivatedRoute,
+              private _location : Location,
               private _recipeListComponent : RecipeListPage
     ) { 
+    this.getRecipeNameFromURL();
+  }
+
+  ngOnInit() {
+  }
+
+  getRecipeNameFromURL() {
     console.log("Params: " + this._route.snapshot.paramMap.get('id'));
     this._recipeListComponent.listOfRecipes.forEach((res) => {
       if (res.id.toString() == this._route.snapshot.paramMap.get('id')) {
@@ -23,7 +32,8 @@ export class RecipeDetailPage implements OnInit {
     });
   }
 
-  ngOnInit() {
+  onBack() {
+    this._location.back();
   }
 
 }
